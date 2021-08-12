2c400196e8c4fae60f4b6790a49f8b49834270f9
Pranav Sastry <pranava.sri@gmail.com>
2021-08-12 13:15:28 +0530

    Home wasnt added to app-d

--- app-d.py
+++ app-d.py
@@ -22,6 +22,12 @@ def objs_to_dict(objs):
     return objs_list
 
 
+@app.get("/")
+@app.get("/home")
+def home():
+    return render_template('main.html')
+
+
 if __name__=='__main__':
     app.jinja_env.cache = {}
     app.run(host=HOST, port=PORT, debug=DEBUG, threaded=True)
